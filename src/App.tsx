610103c4108081a75fcef5787c3f582789a8959e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 17:58:32 +0100

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from 'components/pages';
 import './App.scss';
 
-// TODO init apollo ici
-
 const App: FunctionComponent = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/dashboard' element={<DashBoard/>} /> */}
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter >
