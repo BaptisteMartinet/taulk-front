4a73bb77d1ae97c84b848c4352c1f8faf1ab5d7f
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 19:06:10 +0100

    feat(components/common): rework header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -2,10 +2,24 @@ import React, { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
+const MinScrollValue = 5;
+
 const Header: FunctionComponent = () => {
+  const [top, setTop] = React.useState(true);
+  React.useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const scrolled = document.scrollingElement?.scrollTop;
+      if (scrolled == null) return;
+      if (scrolled >= MinScrollValue) {
+        setTop(false);
+      } else {
+        setTop(true);
+      }
+    });
+  });
   return (
     <header>
-      <div className={styles.container}>
+      <div className={styles.container} style={{ background: top ? 'transparent' : 'rgba(0, 0, 0, 0.8)' }}>
         <div className={styles.logoContainer}>
           <a href="/">
             <img src="assets/logos/logo_white.png" alt="logo" />
@@ -13,8 +27,9 @@ const Header: FunctionComponent = () => {
         </div>
         <nav aria-label="Menu">
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="#lobbies">Lobbies</Link></li>
+            <li><a href="/#">Home</a></li>
+            <li><a href="/#learn">Learn</a></li>
+            <li><a href="/#lobbies">Lobbies</a></li>
             <li><a href="https://github.com/BaptisteMartinet" target="_blanck">Github</a></li>
           </ul>
         </nav>
