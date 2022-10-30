82f7488fe1ae14f3e2a784dc6d6a11169e68f28a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 21:02:38 +0100

    feat: more translations

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const MinScrollValue = 5;
 
 const Header: FunctionComponent = () => {
+  const { t } = useTranslation();
   const [top, setTop] = React.useState(true);
   React.useEffect(() => {
     document.addEventListener('scroll', () => {
@@ -27,13 +29,13 @@ const Header: FunctionComponent = () => {
         </div>
         <nav aria-label="Menu">
           <ul>
-            <li><a href="/#">Home</a></li>
-            <li><a href="/#learn">Learn</a></li>
-            <li><a href="/#lobbies">Lobbies</a></li>
+            <li><a href="/#">{t('common.header.nav.home')}</a></li>
+            <li><a href="/#learn">{t('common.header.nav.about')}</a></li>
+            <li><a href="/#lobbies">{t('common.header.nav.lobbies')}</a></li>
             <li><a href="https://github.com/BaptisteMartinet" target="_blanck">Github</a></li>
           </ul>
         </nav>
-        <Link to="/login" className={styles.openBtn}>Open App</Link>
+        <Link to="/login" className={styles.openBtn}>{t('common.header.openApp')}</Link>
       </div>
     </header >
   );
