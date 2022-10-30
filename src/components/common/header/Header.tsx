96bbbbd7b080fda1b32021fa587e852f48297d0c
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 03:13:31 +0100

    feat(components/common): rework header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './header.module.scss';
+import styles from './Header.module.scss';
 
 const Header: FunctionComponent = () => {
   return (
@@ -13,20 +13,14 @@ const Header: FunctionComponent = () => {
         </div>
         <nav aria-label="Menu">
           <ul>
-            <li><Link to="/">Get Started</Link></li>
-            <li><Link to="/">Lobbies</Link></li>
-            <li><a href="https://github.com/BaptisteMartinet">Github</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="#lobbies">Lobbies</Link></li>
+            <li><a href="https://github.com/BaptisteMartinet" target="_blanck">Github</a></li>
           </ul>
         </nav>
-        <nav aria-label="Other links">
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+        <Link to="/login" className={styles.openBtn}>Open App</Link>
       </div>
-    </header>
+    </header >
   );
 };
 
