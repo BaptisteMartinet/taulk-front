0e6f6b24feb82eea99fca6dfe5d67af0b1cbc5c1
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-31 00:09:47 +0100

    feat(components/common): create LanguagePicker component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Footer } from './footer';
 export { default as Header } from './header';
 export { default as ScrollMouse } from './scroll-mouse';
+export { default as LanguagePicker } from './language-picker';
---
+++ src/components/common/language-picker/LanguagePicker.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+import i18next from 'i18next';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import i18n, { Language } from 'i18n';
+
+const LanguagePicker: FunctionComponent = () => {
+  const [lang, setLang] = React.useState(i18next.language as Language);
+
+  const handleChange = (event: SelectChangeEvent): void => {
+    const newLang = event.target.value as Language;
+    i18n.changeLanguage(newLang).catch(() => { });
+    setLang(newLang);
+  };
+  return (
+    <Box sx={{ minWidth: 80 }}>
+      <FormControl fullWidth>
+        <Select
+          id="language-select"
+          value={lang}
+          onChange={handleChange}
+          variant="standard"
+        >
+          <MenuItem value={Language.EN}>EN</MenuItem>
+          <MenuItem value={Language.FR}>FR</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default LanguagePicker;
