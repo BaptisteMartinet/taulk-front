2e6d7d88126e113dcc6566e34aba33bcc87f7ed7
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 16:02:57 +0100

    feat: init apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', // TODO handle env var
+  cache: new InMemoryCache(),
+  name: 'wesbite',
+  version: '1.0',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>);
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+);
