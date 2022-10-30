2ed70f4e115dab5fa45e5f205c4ca6c8e83d120e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 17:57:45 +0100

    feat(api): add api types

---
+++ src/api/types/index.ts
@@ -0,0 +1,51 @@
+export interface UserRestricted {
+  username: string
+}
+
+export interface User {
+  id: string
+  username: string
+  email: string
+  createdAt: Date
+  updatedAt: Date
+  // token (to be removed)
+}
+
+export interface Message {
+  id: string
+  channel: string // TODO reference ChannelType
+  owner: UserRestricted
+  text: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Channel {
+  id: string
+  lobby: string // TODO reference LobbyType
+  title: string
+  owner: User
+  users: UserRestricted[]
+  messages: Message[]
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface LobbyRestricted {
+  id: string
+  title: string
+  description: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Lobby {
+  id: string
+  owner: UserRestricted
+  title: string
+  description: string
+  channels: Channel[]
+  users: UserRestricted[]
+  createdAt: Date
+  updatedAt: Date
+}
