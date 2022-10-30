ea58109ebf64788fd8068b34743572a75df4a68a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-30 15:49:57 +0100

    feat(components/common): footer basis

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React, { FunctionComponent } from 'react';
+import styles from './Footer.module.scss';
 
 const Footer: FunctionComponent = () => {
-  return <></>;
+  return (
+    <footer>
+      <div className={styles.container}></div>
+    </footer>
+  );
 };
 
 export default Footer;
