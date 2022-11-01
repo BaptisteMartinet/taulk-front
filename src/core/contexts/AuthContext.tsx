ea9e27cefc251d0a329006ae2488ea1f278defe7
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-11-01 16:32:18 +0100

    feat(core/contexts): auto fetch user

--- src/core/contexts/AuthContext.tsx
+++ src/core/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, PropsWithChildren } from 'react';
+import { gql, useQuery } from '@apollo/client';
 import { User } from 'core/api/types';
 
 interface AuthContextT {
@@ -11,11 +12,30 @@ const AuthContext = React.createContext<AuthContextT>({
   login: () => { },
 });
 
+const GetAccount = gql`
+query GetAccount {
+  authenticated {
+    account {
+      id
+      username
+      email
+      createdAt
+      updatedAt
+    }
+  }
+}
+`;
+
 export const AuthContextProvider: FunctionComponent<PropsWithChildren<{}>> = (props) => {
   const [user, setUser] = React.useState<User | null>(null);
   const login = (_user: User): void => {
     setUser(_user);
   };
+  useQuery(GetAccount, {
+    onCompleted: (data) => {
+      setUser(data.authenticated.account as User);
+    },
+  });
   return (
     <AuthContext.Provider value={{ user, login }}>
       {props.children}
